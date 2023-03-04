c8d451a88c6ff4a33b5aeebf1ce58ab234329d13
Zachari THIRY <zachari.thiry@gmail.com>
2023-03-04 10:42:24 +0100

    Added tournement selection

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 def get_distance(d, p1, p2):
     """
@@ -10,3 +11,8 @@ def get_distance(d, p1, p2):
         The Second patient
     """
     return d[p1.id][p2.id]
+
+def tournament_selection(population,p, k=3):
+    tournament = population.sample(k)
+    winner = min(tournament, key=lambda x: x.fitness(p)) # Calculate fitness using fitness_func
+    return winner
