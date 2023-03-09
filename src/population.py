21b9c04e385555d6fc63e516f431dfbb25285de3
Zachari THIRY <zachari.thiry@gmail.com>
2023-03-09 13:36:17 +0100

    added population mutation

--- src/population.py
+++ src/population.py
@@ -1,9 +1,17 @@
 import numpy as np
 from utils import get_distance, generate_sub_arrays, tournament_selection
 import random
+from loader import Problem
 
 class Route:
+    """
+    self.patients : python list of dtype Patient
+    self.ids : np.array of ids (used for computation, not representation)
+    """
     def __init__(self, patients) -> None:
+        """
+        Takes as input : patients, the arrat of dtype Patient
+        """
         self.patients = patients
         self.ids = np.array([patient.id for patient in patients])
 
@@ -36,13 +44,20 @@ class Route:
         return time
 
 class Solution:
-    def __init__(self, p) -> None:
-        route_indexes = generate_sub_arrays(p.nbr_patients, p.nbr_nurses)
+    def __init__(self, p, route_indexes:np.ndarray = None) -> None:
+        """ 
+        Parameters
+        ----------
+        Route_indexes : np.ndarray(dtype=int)
+            a list of id for the routes
+        """
+        # If roue_indexes is not provided, build according to problem parameters. Else, use route_indexes
+        route_indexes = generate_sub_arrays(p.nbr_patients, p.nbr_nurses) if route_indexes == None else route_indexes
         routes = []
         for route in route_indexes:
             route = Route([p.patients[patient_id] for patient_id in route])
             routes.append(route)
-        self.routes = routes
+        self.routes = np.array(routes, dtype=object)
         self.matrix = route_indexes
 
     def __len__(self):
@@ -77,6 +92,9 @@ class Population:
         if init == "custom" : assert solutions is not None, "Solutions `solutions`must be provided on custom init"
         if init == "random" : self.solutions = [Solution(p) for _ in range(pop_size)]
         elif init == "custom" : self.solutions = solutions
+
+        self.pop_size = pop_size
+        self.problem = p
             
     def __getitem__(self, index):
         return self.solutions[index]
@@ -97,7 +115,7 @@ class Population:
     def sample(self, k):
         return random.sample(self.solutions, k)
     
-
+# Parent selection
 def get_parents(population, p):
     pop_size = len(population)
     parents = []
@@ -105,3 +123,98 @@ def get_parents(population, p):
         parent = tournament_selection(population, p, k=3)
         parents.append(parent)
     return Population(pop_size, p, init="custom", solutions=parents)
+
+# Offspring creation
+
+def mutate_population(population:Population,problem:Problem):
+    """Takes as input a population, selects parents, applies crossver between different solutions, and then mutates each solution.
+    """
+    new_pop = get_parents(population, problem)
+    solutions = []
+    for solution in new_pop :
+        new_solution = Solution(problem, mutate_solution(solution))
+        solutions.append(new_solution)
+    return Population(population.pop_size, population.problem, init='custom', solutions = solutions)
+
+def mutate_solution(s:Solution, n_mutations:int =3):
+    """Does a crossover over a random selection of 2 routes from the solution,
+    and does `n_mutations` within random roads.
+        
+    Returns
+    -------
+        new_route_indexes : list(list(ids))
+            A new solution's route_indexes with effectuated random_crossover.
+    """
+    new_route_indexes = np.copy(s.matrix)
+
+    i,j = np.random.choice(len(new_route_indexes), 2)
+    new_route_indexes[i], new_route_indexes[j] = intra_cross_over(new_route_indexes[i], new_route_indexes[j])
+
+    mut_idx = np.random.choice(len(new_route_indexes), n_mutations)
+    for idx in mut_idx :
+        new_route_indexes[idx] = inverse_mutation(new_route_indexes[idx])
+
+    return new_route_indexes
+
+def inverse_mutation(parent):
+    """ Takes as input a route p1 and performs in-place inverse mutation
+    """
+    L = len(parent)
+    # If parent is too short, then don't inverse_mutate
+    if L <= 1: 
+        return parent
+    
+    # If length is sufficient, do a mutation
+    else :
+        i,j = np.random.choice(L, 2, replace=False)
+        if i > j:
+            i, j = j, i
+
+        # Invert the segment between i and j
+        parent[i:j+1] = parent[i:j+1][::-1]
+
+        return parent
+
+
+def intra_cross_over(p1,p2):
+    """Does a crossover, within the routes of a same solution. Is called by intra_cross_over()
+
+    Parameters
+    ----------
+        p1 : list(int)
+            The list of ids of parent 1
+        p2 : list(int)
+            The list of ids of parent 2
+    Returns
+    -------
+    list(ids),lsit(ids)
+        The ids corresponding to the routes of child 1 and child 2
+        
+    """
+
+    l1, l2 = len(p1), len(p2)
+
+    if l1 == 0 or l2 == 0 : return p1,p2 # If one route is empty, return the parents (crossover impossible)
+
+    c1 = np.empty(l1, dtype=int)
+    c2 = np.empty(l2, dtype=int)
+    if not l1 > l2 : 
+        p1,p2 = p2,p1 # Making sure p1 is the longer route
+        c1,c2 = c2, c1
+    min_length = min(l1, l2)
+    x_over_point = np.random.randint(0,min_length)
+    c1[:x_over_point] = p2[:x_over_point]
+    c1[x_over_point:] = p1[x_over_point:]
+    c2[:x_over_point] = p1[:x_over_point]
+    c2[x_over_point:] = p2[x_over_point:]
+
+    return c1, c2
+
+    # # Reconstruct patients
+    # c1_patients = [all_patients[np.where(all_ids == id)[0][0]] for id in c1]
+    # c2_patients = [all_patients[np.where(all_ids == id)[0][0]] for id in c2]
+    
+    # # c1_patients = 
+    # return Route(c1_patients), Route(c2_patients)
+
+# Offspring surviving
